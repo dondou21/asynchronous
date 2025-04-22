7ee38a757a3e621c36a9406ffafabe0022516842
Gym Uruhimbi <gymuruhimbi@uruhimbis-iMac.local>
2025-04-22 13:30:10 +0200

    Promises , resolve,reject,then and catch

---
+++ scripts/promise.js
@@ -0,0 +1,30 @@
+
+/*
+const promise = new Promise((resolve, reject) =>{
+    const allWentWell = false;
+
+    if(allWentWell) {
+        resolve('All things wwent well!')
+    } else {
+        reject('Opps! Something went wrong!')
+    }
+});
+*/
+
+const promise = new Promise((resolve, reject) => {
+    const randomNumber = Math.floor(Math.random() * 10);
+
+    setTimeout(() => {
+        if(randomNumber < 4) {
+            resolve('Well Done!!!')
+        }else {
+            reject('Oops! You Guessed Wrong!!!, Sorry')
+        }
+    })
+});
+
+promise.then((value) => {
+    console.log(value)
+}).catch((error) => {
+    console.log(error)
+});
