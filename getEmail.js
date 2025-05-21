ca8242601e3ef940cfe83dd637c958c49c672985
Gym Uruhimbi <gymuruhimbi@uruhimbis-iMac.local>
2025-05-21 15:19:24 +0200

    get the user email end with .bz using async/await

--- getEmail.js
+++ getEmail.js
@@ -1,11 +1,23 @@
-function getBizEmails() {
-    return fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => users.filter(user => user.email.endsWith('.biz'))
-.map(user => user.email))
+// function getBizEmails() {
+//     return fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(response => response.json())
+//     .then(users => users.filter(user => user.email.endsWith('.biz'))
+// .map(user => user.email))
 
-    .catch(error => console.error("Error:", error.message))
+//     .catch(error => console.error("Error:", error.message))
+// }
+
+// getBizEmails().then(emails => console.log(emails))
+
+async function getUsersEmails() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const users = await response.json();
+
+        return users.map(user => user.email);
+} catch (error) {console.error("Error:". error.message);
+    return [];
+}
 }
 
-getBizEmails().then(emails => console.log(emails))
-
+getUsersEmails().then(emails => console.log(emails));
